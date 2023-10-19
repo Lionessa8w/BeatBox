fd668420e485db3df88bf1e2927a3fa255aa685e
Marina <marina8ww1@gmail.com>
2023-10-19 14:02:14 +0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.beatbox
+
+import android.content.res.AssetManager
+
+private const val TAG="BeatBox"
+private const val SOUNDS_FOLDER="simple_sounds"
+
+/**
+ * AssetManager используется для обращения к активам
+ */
+
+class BeatBox (private val assets:AssetManager){
+}
