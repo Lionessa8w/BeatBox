1944ace57b5d09c33878a56f1ec09a943ca9683b
Marina <marina8ww1@gmail.com>
2023-10-23 14:26:31 +0700

    23.10 class SoundViewModelTest.kt

--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
@@ -10,9 +10,12 @@ import javax.security.auth.Subject
 class SoundViewModelTest {
     private lateinit var sound: Sound
     private lateinit var subject: SoundViewModel
+    private lateinit var beatBox: BeatBox
+
 
     @Before
     fun setUp() {
+        beatBox = mock(BeatBox::class.java)
         sound = Sound("assetPath")
         subject = SoundViewModel()
         subject.sound = sound
@@ -22,5 +25,13 @@ class SoundViewModelTest {
     fun exposesSoundNameAsTitle() {
         assertEquals(subject.title, sound.name)
 
+    }
+
+    /**
+     * Ваш тест вызывает функцию и проверяет, делает ли она то, что ей сказано: вызывает BeatBox.play(Sound).
+     */
+    @Test
+    fun callsBeatBoxPlayOnButtonClicked() {
+        subject.onButtonClicked()
     }
 }
